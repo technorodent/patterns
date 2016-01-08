c161e0f251641f265f0db2f559bda0bea9ba1d91
Robert Warren <r.warren@wheatbridge.com>
2016-01-07 17:01:07 -0700

    Build Jade Templates:         footer.html         front-page-opener.html         front-page-recent-entries.html         head.html         index.jade  Added style sheets + images + js scripts  //NOTE: did not add fonts (copyrights)

---
+++ public/js/custom.js
@@ -0,0 +1,184 @@
+/*-----------------------------------------------------------------------------------
+/*
+/* Custom JS
+/*
+-----------------------------------------------------------------------------------*/
+
+/* Start Document */
+jQuery(document).ready(function() {
+
+/*----------------------------------------------------*/
+/*	Main Navigation
+/*----------------------------------------------------*/
+
+	/* Menu */
+	(function() {
+
+		var $mainNav    = $('#navigation').children('ul');
+
+		$mainNav.on('mouseenter', 'li', function() {
+			var $this    = $(this),
+				$subMenu = $this.children('ul');
+			if( $subMenu.length ) $this.addClass('hover');
+			$subMenu.hide().stop(true, true).fadeIn(250);
+		}).on('mouseleave', 'li', function() {
+			$(this).removeClass('hover').children('ul').stop(true, true).fadeOut(50);
+		});
+		
+	})();
+	
+	/* Responsive Menu */
+	domready(function(){
+			
+		selectnav('nav', {
+			label: 'Menu',
+			nested: true,
+			indent: '-'
+		});
+				
+	});
+
+/*----------------------------------------------------*/
+/*	Isotope Portfolio Filter
+/*----------------------------------------------------*/
+	$(window).load(function(){
+		$('#portfolio-wrapper').isotope({
+			  itemSelector : '.portfolio-item',
+				layoutMode : 'fitRows'
+		});
+		$('#filters a.selected').trigger("click");
+	});
+	$('#filters a').click(function(e){
+		e.preventDefault();
+
+		var selector = $(this).attr('data-option-value');
+		$('#portfolio-wrapper').isotope({ filter: selector });
+
+		$(this).parents('ul').find('a').removeClass('selected');
+		$(this).addClass('selected');
+	});
+
+
+/*----------------------------------------------------*/
+/*	Back To Top Button
+/*----------------------------------------------------*/
+
+	(function() {
+		var pxShow = 300;//height on which the button will show
+		var fadeInTime = 400;//how slow/fast you want the button to show
+		var fadeOutTime = 400;//how slow/fast you want the button to hide
+		var scrollSpeed = 400;//how slow/fast you want the button to scroll to top. can be a value, 'slow', 'normal' or 'fast'
+
+		jQuery(window).scroll(function(){
+			if(jQuery(window).scrollTop() >= pxShow){
+				jQuery("#backtotop").fadeIn(fadeInTime);
+			}else{
+				jQuery("#backtotop").fadeOut(fadeOutTime);
+			}
+		});
+		 
+		jQuery('#backtotop a').click(function(){
+			jQuery('html, body').animate({scrollTop:0}, scrollSpeed); 
+			return false; 
+		}); 
+	})();
+
+/*----------------------------------------------------*/
+/*	Contact Form
+/*----------------------------------------------------*/
+
+(function() {
+var animateSpeed=300;
+var emailReg = /^[a-zA-Z0-9._-]+@([a-zA-Z0-9.-]+\.)+[a-zA-Z0-9.-]{2,4}$/;
+
+	// Validating
+	
+	function validateName(name) {
+		if (name.val()=='*') {name.addClass('validation-error',animateSpeed); return false;}
+		else {name.removeClass('validation-error',animateSpeed); return true;}
+	}
+
+	function validateEmail(email,regex) {
+		if (!regex.test(email.val())) {email.addClass('validation-error',animateSpeed); return false;}
+		else {email.removeClass('validation-error',animateSpeed); return true;}
+	}
+		
+	function validateMessage(message) {
+		if (message.val()=='') {message.addClass('validation-error',animateSpeed); return false;}
+		else {message.removeClass('validation-error',animateSpeed); return true;}
+	}
+                
+	$('#send').click(function() {
+	
+		var result=true;
+		
+		var name = $('input[name=name]');
+		var email = $('input[name=email]');
+		var message = $('textarea[name=message]');
+                
+		// Validate
+		if(!validateName(name)) result=false;
+		if(!validateEmail(email,emailReg)) result=false;
+		if(!validateMessage(message)) result=false;
+		
+		if(result==false) return false;
+				
+		// Data
+		var data = 'name=' + name.val() + '&email=' + email.val() + '&message='  + encodeURIComponent(message.val());
+		
+		// Disable fields
+		$('.text').attr('disabled','true');
+		
+		// Loading icon
+		$('.loading').show();
+		
+		// Start jQuery
+		$.ajax({
+		
+			// PHP file that processes the data and send mail
+			url: "contact.php",	
+			
+			// GET method is used
+			type: "GET",
+
+			// Pass the data			
+			data: data,		
+			
+			//Do not cache the page
+			cache: false,
+			
+			// Success
+			success: function (html) {				
+			
+				if (html==1) {	
+
+					// Loading icon
+					$('.loading').fadeOut('slow');	
+						
+					//show the success message
+					$('.success-message').slideDown('slow');
+											
+					// Disable send button
+					$('#send').attr('disabled',true);
+					
+				}
+				
+				else {
+					$('.loading').fadeOut('slow')
+					alert('Sorry, unexpected error. Please try again later.');				
+				}
+			}		
+		});
+	
+		return false;
+		
+	});
+		
+	$('input[name=name]').blur(function(){validateName($(this));});
+	$('input[name=email]').blur(function(){validateEmail($(this),emailReg); });
+	$('textarea[name=message]').blur(function(){validateMessage($(this)); });
+       
+})();
+
+/* End Document */
+});
---
+++ public/js/main.js
@@ -0,0 +1,18 @@
+//integrate menu
+/*
+jQuery(function ($) {
+    $('.menu-btn').click(function () {
+        $('.responsive-menu').addClass('expand');
+        $('.menu-btn').addClass('btn-none');
+    });
+    $('.close-btn').click(function () {
+        $('.responsive-menu').removeClass('expand');
+        $('.menu-btn').removeClass('btn-none');
+    });
+});
+*/
+jQuery(function ($) {
+    $('.menu-btn').click(function () {
+        $('.responsive-menu').toggleClass('expand')
+    })
+});
