a679038837b6351d122ff2e064edba2ca5fe64cb
Robert Warren <r.warren@wheatbridge.com>
2016-01-09 22:59:31 -0700

    Saturday Closeout

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,123 @@
+//Below is a totally awesome polyfill fix for the DOMParser
+//without it the DOMParser returned element is null
+/*
+ * DOMParser HTML extension
+ * 2012-02-02
+ *
+ * By Eli Grey, http://eligrey.com
+ * Public domain.
+ */
+
+/*! @source https://gist.github.com/1129031 */
+/*global document, DOMParser*/
+var currentState = '';
+var bodyGrp = document.getElementById("bodyGrp");
+var elem, removalArray = [];
+var sPre = "     -- ";
+var sPreFail = "     xx ";
+getBinary = function (strFrag) {
+    var res = [];
+    strFrag.split('').forEach(function (letter) {
+        var bin = letter.charCodeAt(0).toString(2),
+            padding = 8 - bin.length;
+        res.push(new Array(padding + 1).join('0') + bin);
+    });
+    var returnString = res.toString();
+    returnString = returnString.replace(/,/g, " ");
+    return returnString;
+};
+(function (DOMParser) {
+    "use strict";
+    var DOMParser_proto = DOMParser.prototype
+        , real_parseFromString = DOMParser_proto.parseFromString;
+    // Firefox/Opera/IE throw errors on unsupported types
+    try {
+        // WebKit returns null on unsupported types
+        if ((new DOMParser).parseFromString("", "text/html")) {
+            // text/html parsing is natively supported
+            return;
+        }
+    } catch (ex) {
+    }
+    DOMParser_proto.parseFromString = function (markup, type) {
+        if (/^\s*text\/html\s*(?:;|$)/i.test(type)) {
+            var doc = document.implementation.createHTMLDocument("")
+                , doc_elt = doc.documentElement
+                , first_elt;
+            doc_elt.innerHTML = markup;
+            first_elt = doc_elt.firstElementChild;
+            if (doc_elt.childElementCount === 1
+                && first_elt.localName.toLowerCase() === "html") {
+                doc.replaceChild(first_elt, doc_elt);
+            }
+            return doc;
+        } else {
+            return real_parseFromString.apply(this, arguments);
+        }
+    };
+}(DOMParser));
 //integrate menu
 /*
-jQuery(function ($) {
-    $('.menu-btn').click(function () {
-        $('.responsive-menu').addClass('expand');
-        $('.menu-btn').addClass('btn-none');
-    });
-    $('.close-btn').click(function () {
-        $('.responsive-menu').removeClass('expand');
-        $('.menu-btn').removeClass('btn-none');
-    });
-});
-*/
+ jQuery(function ($) {
+ $('.menu-btn').click(function () {
+ $('.responsive-menu').addClass('expand');
+ $('.menu-btn').addClass('btn-none');
+ });
+ $('.close-btn').click(function () {
+ $('.responsive-menu').removeClass('expand');
+ $('.menu-btn').removeClass('btn-none');
+ });
+ });
+ */
 jQuery(function ($) {
     $('.menu-btn').click(function () {
         $('.responsive-menu').toggleClass('expand')
     })
 });
+// Store the initial content so we can revisit it later
+updateContent = function(data) {
+    if (data == null)
+        return;
+    bodyGrp.innerHTML = data.pgHTML;
+}
+window.addEventListener('popstate', function (event) {
+    updateContent(event.state);
+});
+window.onload = function () {
+    var htmlPage = document.getElementById("bodyGrp");
+    var popObj = {pgHTML: htmlPage.innerHTML, pgTitle: ''};
+    history.replaceState(popObj, document.title, document.location.href);
+}
+$('#responsive-menu').on('click', 'li', function (ev) {
+    var rObj = ev.target.getAttribute("loc");
+    if (rObj) {
+        var url = ev.target.attributes["loc"].value;
+        loadPattern(url);
+        ev.stopPropagation();
+        ev.preventDefault();
+    }
+});
+loadPattern = function(url){
+    var xhttp = new XMLHttpRequest();
+    var params = "type=select";
+    xhttp.open("POST", url, true);
+    console.log(url);
+    xhttp.targetAddr = url;
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.onreadystatechange = function () {
+        if (xhttp.readyState == 4) {
+            if (xhttp.status != 200) {
+                console.log("error")
+            } else {
+                $('.responsive-menu').toggleClass('expand');
+                var parser = new DOMParser();
+                var el = parser.parseFromString(xhttp.responseText, "text/html");
+                var htmlPage = el.innerHTML;
+                var popObj = {pgHTML: xhttp.responseText, pgTitle: xhttp.targetAddr};
+                updateContent(popObj);
+                window.history.pushState(popObj, xhttp.targetAddr, xhttp.targetAddr);
+            }
+        }
+    };
+    xhttp.send(params);
+}
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,83 @@
 var express = require('express');
 var router = express.Router();
-var validate = require('./rest-get.js');
+var mysql = require('mysql');
+var pool = mysql.createPool({
+    connectionLimit: 100,
+    host: 'localhost',
+    user: '',
+    password: '',
+    database: 'pattern_library',
+    debug: false
+});
+//var validate = require('./rest-get.js');
 /* GET home page. */
+
+//----------------POSTS---------------------
+router.post('/general-patterns/:pattern', function (req, res, next) {
+    getPostPage('/general-patterns/', req, res);
+    //res.render('patternFragmentPage');
+    //res.end('{"success" : "Updated Successfully", "status" : 200}');
+});
+router.post('/general-patterns/:pattern', function (req, res, next) {
+    getPostPage('/general-patterns/', req, res);
+});
+router.post('/jquery/general/:pattern', function (req, res) {
+    getPostPage('/jquery/general/', req.params.pattern, 1);
+    console.log("here");
+});
+router.post('/jquery/selector/:pattern', function (req, res) {
+    getPostPage('/jquery/selector/', req.params.pattern, 1);
+});
+router.post('/jquery/publish-subscribe/:pattern', function (req, res) {
+    getPostPage('/jquery/publish-subscribe/', req.params.pattern, 1);
+});
+router.post('/jquery/plugin/:pattern', function (req, res) {
+    getPostPage('/jquery/plugin/', req.params.pattern, 1);
+});
+router.post('/literals-constructors/:pattern', function (req, res) {
+    getPostPage('/literals/constructors/', req.params.pattern, 1);
+});
+router.post('/functions/:pattern', function (req, res) {
+    getPostPage('/functions/', req.params.pattern, 1);
+});
+router.post('/functions/api/:pattern', function (req, res) {
+    getPostPage('/functions/api/', req.params.pattern, 1);
+});
+router.post('/functions/initialization/:pattern', function (req, res) {
+    getPostPage('/functions/initialization/', req.params.pattern, 1);
+});
+router.post('/functions/performance/:pattern', function (req, res) {
+    getPostPage('/functions/performance/', req.params.pattern, 1);
+});
+router.post('/object-creation/:pattern', function (req, res) {
+    getPostPage('/object-creation/', req.params.pattern, 1);
+});
+router.post('/code-reuse/:pattern', function (req, res) {
+    getPostPage('/code-reuse/', req.params.pattern, 1);
+});
+router.post('/code-reuse/classical/:pattern', function (req, res) {
+    getPostPage('/code-reuse/classical/', req.params.pattern, 1);
+});
+router.post('/code-reuse/preferred/:pattern', function (req, res) {
+    getPostPage('/code-reuse/preferred/', req.params.pattern, 1);
+});
+router.post('/design/:pattern', function (req, res) {
+    getPostPage('/design/', req.params.pattern, 1);
+});
+router.post('/design/creational/:pattern', function (req, res) {
+    getPostPage('/design/creational/', req.params.pattern, 1);
+});
+router.post('/design/structural/:pattern', function (req, res) {
+    getPostPage('/design/structural/', req.params.pattern, 1);
+});
+router.post('/design/behavioral/:pattern', function (req, res) {
+    getPostPage('/design/behavioral/', req.params.pattern, 1);
+});
+//----------------GETS---------------------
 router.get('/', function (req, res, next) {
     // res.send('Hello World!');
     res.render('index', {title: 'Express'});
 });
-
-
 router.get('/about', function (req, res) {
     res.send('How about that');
 });
@@ -15,7 +85,7 @@ router.get('/general-patterns/', function (req, res) {
     res.send("general-patterns");
 });
 router.get('/general-patterns/:pattern', function (req, res) {
-    console.log(req.params.pattern);
+    //console.log(req.params.pattern);
     getPage('/general-patterns/', req, res);
 });
 router.get('/jquery/general/:pattern', function (req, res) {
@@ -67,12 +137,10 @@ router.get('/code-reuse/classical/:pattern', function (req, res) {
     res.send("ok");
 });
 router.get('/code-reuse/preferred/:pattern', function (req, res) {
-
     getPage('/code-reuse/preferred/', req.params.pattern);
     res.send("ok");
 });
 router.get('/design/:pattern', function (req, res) {
-
     getPage('/design/', req.params.pattern);
     res.send("ok");
 });
@@ -81,7 +149,6 @@ router.get('/design/creational/:pattern', function (req, res) {
     res.send("ok");
 });
 router.get('/design/structural/:pattern', function (req, res) {
-
     getPage('/design/structural/', req.params.pattern);
     res.send("ok");
 });
@@ -89,24 +156,90 @@ router.get('/design/behavioral/:pattern', function (req, res) {
     getPage('/design/behavioral/', req.params.pattern);
     res.send("ok");
 });
-getPage = function(organizer, request, response){
-    var mysql = require('mysql');
-    var returnVal = 0;
-    var connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: '~WESMPC1!a',
-        database: 'pattern_library'
+/*
+testDB = function (organizer, oAddress) {
+    pool.getConnection(function (err, connection) {
+        if (err) {
+            connection.release();
+            console.log(err);
+        }
+        //console.log('connected as id ' + connection.threadId);
+        var oPromise = new Promise(function (resolve, reject) {
+            console.log("Select pk as keyVal from patterns where route_a like '" + organizer + "' and route_b like'" + oAddress + "'");
+            connection.query("Select pk as keyVal from patterns where route_a like '" + organizer + "' and route_b like '" + oAddress + "'", function (err, rows, fields) {
+                if (!err) {
+                    if (typeof rows[0] == 'undefined') {
+                        console.log("Undefined: " + organizer + " " + oAddress);
+                        connection.release();
+                        reject("It failed");
+                    } else {
+                        connection.release();
+                        console.log("....itemKey: " + rows[0].keyVal);
+                        resolve(rows[0].keyVal);
+                    }
+                }
+                else {
+                    console.log("failure");
+                    connection.release();
+                    reject("It failed");
+                }
+            });
+        });
+        connection.on('error', function (err) {
+            console.log("connection.on error: " + err.message);
+        });
     });
+}
+var restMe = new Array("function-declarations", "conditionals", "global-object-access", "single-var", "hoisting", "for-loop", "for-in-loop", "augment-built-in-prototypes", "switch", "implied-typecasting", "eval", "number-conversion", "globals", "globals");
+for (var i = 0; i < restMe.length; i++) {
+    console.log(restMe[i]);
+    testDB("/general-patterns/", restMe[i]);
+}
+*/
+var getPostPage = function (organizer, req, res, level) {
+    var itemKey;
+    pool.getConnection(function (err, connection) {
+        if (err) {
+            connection.release();
+            console.log(err);
+        }
+        //console.log('connected as id ' + connection.threadId);
+        var oPromise = new Promise(function (resolve, reject) {
+            connection.query("Select pk as keyVal from patterns where route_a like '" + organizer + "' and route_b like'" + req.params.pattern + "'", function (err, rows, fields) {
+                //if (err) throw err;
+                if (!err) {
+                    console.log("....itemKey: " + rows[0].keyVal);
+                    resolve(rows[0].keyVal);
+                }
+                else {
+                    connection.release();
+                    reject("It failed");
+                }
+            });
+        });
+        oPromise.then(function (result) {
+            connection.query("Select * from patterns where pk=" + result, function (err, rows, fields) {
+                if (err) throw err;
+                res.render('patternFragmentPage', {title: rows[0].pattern}, function (err, html) {
+                    if (err)throw err;
+                    res.send(html);
+                    connection.release();
+                });
+            });
+        }, function (err) {
+            connection.release();
+        });
+    });
+};
+var getPage = function (organizer, req, res) {
+    var returnVal = 0;
     connection.connect();
-    connection.query("Select count(*) as value from patterns where route_a like '" + organizer + "' and route_b like'" + request.params.pattern + "'", function (err, rows, fields) {
+    connection.query("Select count(*) as value from patterns where route_a like '" + organizer + "' and route_b like'" + req.params.pattern + "'", function (err, rows, fields) {
         if (err) throw err;
         console.log("now returning: " + rows[0].value);
         if (rows[0].value == 1) {
             response.render('patternPage', {title: 'Test'});
         }
-
-
     });
     connection.end();
     return returnVal;
@@ -123,5 +256,4 @@ getPage = function(organizer, request, response){
  console.log("--------" + i + "----------");
  }
  */
-
 module.exports = router;
